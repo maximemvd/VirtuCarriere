0199ed2adf1df4954bd0d0f970c22d2fe652a111
Maxime Miville Deschênes <maxime.miville-deschenes.1@ulaval.ca>
2020-02-20 15:35:11 -0500

    Commit

---
+++ virtuCarriereApplication/virtuCarriere/src/virtucarriere/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virtucarriere;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author maximemivilledeschenes
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        virtucarriere.gui.MainWindow mainWindow = new virtucarriere.gui.MainWindow();
+        mainWindow.setExtendedState(mainWindow.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        mainWindow.setVisible(true);
+    }
+    
+}
