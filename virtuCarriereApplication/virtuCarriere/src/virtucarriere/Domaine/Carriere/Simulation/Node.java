5df1c0c0d6e9c315a7666ce0a1343032356e20f2
Vincent Masse <vincent.masse.4@ulaval.ca>
2020-03-03 15:44:30 -0500

    Carriere packages and classes

---
+++ virtuCarriereApplication/virtuCarriere/src/virtucarriere/Domaine/Carriere/Simulation/Node.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virtucarriere.Domaine.Carriere.Simulation;
+
+/**
+ *
+ * @author vincentmasse
+ */
+public class Node {
+    
+}
