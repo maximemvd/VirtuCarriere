ee3e29bcab10356bbf8396ba5dda29aca8ef5d7f
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2020-03-19 17:20:04 -0400

    set up array list

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Controller/ElementContainer.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Controller/ElementContainer.java
@@ -9,9 +9,11 @@ import virtucarriere.Domaine.Carriere.Plan.Element;
 import virtucarriere.Domaine.Carriere.Simulation.Vehicule;
 import virtucarriere.Domaine.Carriere.Simulation.Camion;
 import virtucarriere.Domaine.Carriere.Simulation.Coordonnees;
+import virtucarriere.Domaine.Carriere.Plan.Noeud;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.String;
 import java.util.ArrayList;
 
 public class ElementContainer {
@@ -21,13 +23,13 @@ public class ElementContainer {
     private ArrayList<Camion> vehiculeList;
 
     public void ElementContainer() {
-        this.elementList = elementList;
-        this.vehiculeList = vehiculeList;
+        this.elementList = new ArrayList<Element>();
+        this.vehiculeList = new ArrayList<Camion>();
     }
 
     ;
 
-    public bool elementIsEmpty() {
+    public boolean elementIsEmpty() {
         boolean returnValue = false;
         if (this.elementList.size() == 0) {
             returnValue = true;
@@ -35,7 +37,7 @@ public class ElementContainer {
         return returnValue;
     }
 
-    public bool VehiculeIsEmpty() {
+    public boolean VehiculeIsEmpty() {
         boolean returnValue = false;
         if (this.vehiculeList.size() == 0) {
             returnValue = true;
@@ -59,10 +61,50 @@ public class ElementContainer {
         return this.vehiculeList.size():
     }
 
+    public void addEquipement(Equipement p_equipement)
+    {
+        this.elementList.add(p_equipement);
+    }
+
     public void addCamion(Jeton jeton, double start, Coordonnees coordonnees)
     {
         Camion newCamion = new Camion(jeton, start, coordonnees);
         this.vehiculeList.add(newCamion)
+    }
+
+    public void addNoeud(double x, double y)
+    {
+     Noeud newNoeud = new Noeud(x, y);
+     this.elementList.add(newNoeud);
+    }
+
+    public String askReason()
+    {
+        return "La raison";
+    }
+
+    public double getNextPosition()
+    {
+        // on appel une fonction de vehicule ici
+        double nextPosition = 10;
+        return nextPosition;
+    }
+
+    public boolean askValidPlace(Coordonnees coord)
+    {
+        if (coord)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean validanceDependanceType()
+    {
+        return true;
     }
 
 
@@ -70,10 +112,8 @@ public class ElementContainer {
 
 
 
-
-
-
-    public void addNoeud(double x, double y) {};
-
-    public double getNextPosition() {};
 }
+
+
+
+
