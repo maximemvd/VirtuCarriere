37c260acb677e71b2231780662773a2fa8901168
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2020-03-19 15:17:25 -0400

    ajout classe controller

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Controller/Controller.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Controller/Controller.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package virtucarriere.Domaine.Controller;
+import virtucarriere.Domaine.Controller.ElementContainer;
 
 /**
  *
  * @author vincentmasse
  */
 public class Controller {
-    
+
+    public double attribute;
+
+    public ElementContainer elementContainer;
+
+    public Controller(ElementContainer elementContainer) {
+        this.elementContainer = elementContainer;
+
+    }
+
+    public void Controller() {
+    }
+
+    public void addNoeud(double x, double y) {
+
+    }
+
+    public void ajouterEquipement(Equipement equipement) {
+        this.equipement = equipement;
+    };
+
+    public ElementContainer getElementContainer() {
+        return elementContainer;
+    }
+
+
 }
