d328830090e501e87b6093f0f0c28f1af47b70b3
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2020-03-21 16:47:54 -0400

    Classe Camion

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Simulation/Camion.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Simulation/Camion.java
@@ -14,21 +14,68 @@ public class Camion extends Vehicule {
 
     ArrayList<Node> itineraireCamion;
 
+    private double start;
+
+    private Jeton jeton;
+
+    private Coordonnees coordonnees;
+
     public Camion() {};
 
     public Camion(Jeton jeton,double start, Coordonnees coordonnees) 
     {
-        createCamion(jeton, start, coordonnees);
+        itineraireCamion = new ArrayList<Node>();
+        this.jeton = jeton;
+        this.start = start;
+        this.coordonnees = coordonnees;
+        //createCamion(jeton, start, coordonnees);
     }
 
-    public void createCamion(Jeton jeton,double start, Coordonnees coordonnees) 
+    public Camion createCamion(Jeton jeton,double start, Coordonnees coordonnees) 
     {
-        
+        Camion newCamion = new Camion(jeton, start, coordonnees);
+        return newCamion;
     };
+
+    public ArrayList<Node> getItineaireList()
+    {
+        return itineraireCamion;
+    }
 
     public void addDestination(Node p_node)
     {
         itineraireCamion.add(p_node);
     };
+
+    public void removeDestination(Node p_node)
+    {
+        try 
+        {
+            itineraireCamion.remove(p_node);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public void editerParams(Jeton jeton,double start, Coordonnees coordonnees)
+    {
+        this.jeton = jeton;
+        this.start = start;
+        this.coordonnees = coordonnees;
+    }
+
+    public void Simulation()
+    {
+        for (Node i: itineraireCamion)
+        {
+            System.out.println(i);
+            //call getShortestPath
+        }
+        
+    }
+
+    
 
 }
