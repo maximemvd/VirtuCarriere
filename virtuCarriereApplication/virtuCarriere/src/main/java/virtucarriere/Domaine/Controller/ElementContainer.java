6432322e325193f8283ef134456c7cb12edfdfd0
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2020-03-21 17:59:23 -0400

    Correction dans ElementCOntainer

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Controller/ElementContainer.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Controller/ElementContainer.java
@@ -23,8 +23,8 @@ public class ElementContainer {
     private ArrayList<Camion> vehiculeList;
 
     public ElementContainer() {
-        elementList = new ArrayList<Element>();
-        vehiculeList = new ArrayList<Camion>();
+        this.elementList = new ArrayList<Element>();
+        this.vehiculeList = new ArrayList<Camion>();
     }
 
     public void switchSelectionStatus(double x, double y, boolean isShiftDown) {
@@ -34,32 +34,32 @@ public class ElementContainer {
     }
 
     public boolean elementIsEmpty() {
-        return elementList.isEmpty();
+        return this.elementList.isEmpty();
     }
 
     public boolean VehiculeIsEmpty() {
-        return vehiculeList.isEmpty();
+        return this.vehiculeList.isEmpty();
     }
 
     public ArrayList<Element> getElementList() {
-        return elementList;
+        return this.elementList;
     }
 
     public ArrayList<Camion> getVehiculeList() {
-        return vehiculeList;
+        return this.vehiculeList;
     }
 
     public double getNumberOfElementsList() {
-        return elementList.size();
+        return this.elementList.size();
     }
 
     public double getNumberOfVehiculeList() {
-        return vehiculeList.size();
+        return this.vehiculeList.size();
     }
 
     public void addElement(Element p_element) {
         try {
-        elementList.add(p_element);
+        this.elementList.add(p_element);
         }
         catch (Exception error)
         {
@@ -69,7 +69,7 @@ public class ElementContainer {
 
     public void removeElement(Element p_element) {
         try {
-            elementList.remove(p_element);
+            this.elementList.remove(p_element);
         } catch (Exception error) {
             System.out.println("Element is not in the list");
         }
@@ -77,40 +77,44 @@ public class ElementContainer {
 
     public void addCamion(Jeton jeton, double start, Coordonnees coordonnees) {
         Camion newCamion = new Camion(jeton, start, coordonnees);
-        vehiculeList.add(newCamion);
+        this.vehiculeList.add(newCamion);
     }
 
     public void removeCamion(Camion p_camion) {
         try {
-            vehiculeList.remove(p_camion);
+            this.vehiculeList.remove(p_camion);
         } catch (Exception error) {
             System.out.println("Vehicule is not in the list");
         }
     }
 
-    public void generateCamionSimulation(Jeton jeton, double depart) {
-        Camion vehiculePretPourSimulation = vehiculeList.get(0);
-        System.out.println(vehiculePretPourSimulation);
-        // appeler fonction pour simulation
-        vehiculeList.remove(vehiculePretPourSimulation);
-    }
+    // public void generateCamionSimulation(Jeton jeton, double depart) {
+    //     Camion vehiculePretPourSimulation = vehiculeList.get(0);
+    //     System.out.println(vehiculePretPourSimulation);
+    //     // appeler fonction pour simulation
+    //     this.vehiculeList.remove(vehiculePretPourSimulation);
+    // }
 
     public String askReason() {
         return "La raison";
     }
 
-    public double getNextPosition() {
-        // on appel une fonction de vehicule ici
-        double nextPosition = 10;
-        return nextPosition;
-    }
+    // public double getNextPosition() {
+    //     double nextPosition = 10;
+    //     return nextPosition;
+    // }
 
     public boolean askValidPlace(Coordonnees coordonnee) {
-        int x = 10;
-        if (x < 10) {
-            return true;
-        } else {
+        double x = coordonnee.getX();
+        double y = coordonnee.getY();
+
+        if (x < 0 && y < 0)
+        {
             return false;
+        }
+        else
+        {
+            return true;
         }
     }
 
