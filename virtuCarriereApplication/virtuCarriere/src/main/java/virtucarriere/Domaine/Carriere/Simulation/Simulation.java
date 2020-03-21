f003e958058742d69f09baed8d24cc2e9dd22576
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2020-03-21 17:08:54 -0400

    class simulation

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Simulation/Simulation.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Simulation/Simulation.java
@@ -4,8 +4,50 @@
  * and open the template in the editor.
  */
 package virtucarriere.Domaine.Carriere.Simulation;
+import java.util.ArrayList;
+import virtucarriere.Domaine.Carriere.Simulation.Camion;
 
 public class Simulation {
-    private String nom;
-    
+
+    private String nameSimulation;
+
+    private  ArrayList<Camion> camionList;
+
+    public Simulation(String name)
+    {
+        this.camionList = new ArrayList<Camion>();
+        this.nameSimulation = name;
+    }
+
+    public void addCamion(Camion p_camion)
+    {
+        this.camionList.add(p_camion);
+    }
+
+    public void removeCamion(Camion p_camion)
+    {
+        try
+        {
+            this.camionList.remove(p_camion);
+        }
+        catch (Exception error)
+        {
+            System.out.println(error);
+        }
+    }
+
+    public void changeName(String p_name)
+    {
+        this.nameSimulation = p_name;
+    }
+
+    public String getName()
+    {
+        return this.nameSimulation;
+    }
+
+    public ArrayList<Camion> getCamionList()
+        {
+            return this.camionList;
+        }
 }
