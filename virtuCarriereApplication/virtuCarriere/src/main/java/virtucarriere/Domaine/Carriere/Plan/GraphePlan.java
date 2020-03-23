8b990b8d5ad4fa86364f4ae77bf1772c3a839562
Vincent Masse <vincent.masse.4@ulaval.ca>
2020-03-23 13:59:54 -0400

    Resolge merge conflict

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Plan/GraphePlan.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Plan/GraphePlan.java
@@ -6,52 +6,64 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class GraphePlan {
+
   private Vector<Noeud> noeuds;
   private Vector<List<Arc>> arcs;
 
-  public void addNoeud(Noeud noeud){
-    if (noeudExiste(noeud)){
+  public void addNoeud(Noeud noeud) {
+    if (noeudExiste(noeud)) {
       throw new RuntimeException("Le noeud existe déja");
     }
     noeuds.add(noeud);
   }
 
-  public void removeNoeud(Noeud noeud){
-    if (noeudExiste(noeud)){
+
+  public void removeNoeud(Noeud noeud) {
+    if (noeudExiste(noeud)) {
       noeuds.remove(noeud);
-    }else {
+    } else {
       throw new RuntimeException("Ce neud n'existe pas");
     }
   }
 
-  public boolean noeudExiste(Noeud noeud){
+  public boolean noeudExiste(Noeud noeud) {
     return noeuds.contains(noeud);
   }
 
-  public void addArc(Arc arc){
-    if(arcExiste(arc)){
+  public void addArc(Arc arc) {
+    if (arcExiste(arc)) {
       throw new RuntimeException("Cet arc existe déjà");
     }
   }
 
-  public void removeArc(Arc arc){
-    if (arcExiste(arc)){
+  public void removeArc(Arc arc) {
+    if (arcExiste(arc)) {
       arcs.remove(arc);
-    }else {
+    } else {
       throw new RuntimeException("Cet arc n'existe pas");
     }
   }
 
-  public boolean arcExiste(Arc arc){
+
+  public boolean arcExiste(Arc arc) {
     return arcs.contains(arc);
   }
 
-  List<Noeud> getAdjacents(Noeud noeud){
+  List<Noeud> getAdjacents(Noeud noeud) {
     List<Noeud> adjacents;
     int index = noeuds.indexOf(noeud);
     adjacents = arcs.elementAt(index).stream()
-        .map(s -> s.getArrival())
+        .map(Arc::getArrival)
         .collect(Collectors.toList());
     return adjacents;
+  }
+
+  double getCost(Noeud source, Noeud destination) {
+    Arc arc = new Arc(source, destination);
+    double cost = 0;
+    if (arcExiste(arc)) {
+      cost = arc.getCost();
+    }
+    return cost;
   }
 }
