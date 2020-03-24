5a7467a3969e758de8d4569b1ba812421025b4d5
Maxime Miville Deschênes <maxime.miville-deschenes.1@ulaval.ca>
2020-03-24 12:03:06 -0400

    Debut UnitConverter

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/AffichageUtil/UnitConverter.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/AffichageUtil/UnitConverter.java
@@ -5,10 +5,69 @@
  */
 package virtucarriere.Domaine.AffichageUtil;
 
+import virtucarriere.gui.MainWindow;
+import static virtucarriere.gui.MainWindow.MeasurementUnitMode.IMPERIAL;
+import static virtucarriere.gui.MainWindow.MeasurementUnitMode.METRIC;
+
 /**
  *
  * @author vincentmasse
  */
 public class UnitConverter {
+    
+    public static double inchToPixel(double inch){
+        //TODO calcul à faire selon la dimension de notre fenêtre
+        return inch;
+    }
+    
+    public static double pixelToInch(double pixel){
+        //TODO calcul à faire selon la dimension de notre fenêtre
+        return pixel;
+    }
+    
+    public static double pixelToMeter(double pixel){
+        //TODO calcul à faire selon la dimension de notre fenêtre
+        return pixel;
+    }
+    
+    public static double inchToMeter(double inch){
+        //TODO calcul à faire selon la dimension de notre fenêtre
+        return inch;
+    }
+    
+    public static double meterToInch(double meter){
+        //TODO calcul à faire selon la dimension de notre fenêtre
+        return meter;
+    }
+    
+    public static double meterToPixel(double meter){
+        //TODO calcul à faire selon la dimension de notre fenêtre
+        return meter;
+    }
+     
+    
+     public static double convertSelectedUnitToPixel(double unit, MainWindow.MeasurementUnitMode mode)
+     {
+         switch(mode){
+             case METRIC:
+                 return meterToPixel(unit);
+                 
+             case IMPERIAL:
+                 return inchToPixel(unit);
+         }
+         return unit;
+     }
+     
+     public static double convertPixelToSelectedUnit(double unit, MainWindow.MeasurementUnitMode mode)
+     {
+         switch(mode){
+             case METRIC:
+                 return pixelToMeter(unit);
+                 
+             case IMPERIAL:
+                 return pixelToInch(unit);
+         }
+         return unit;
+     }
     
 }
