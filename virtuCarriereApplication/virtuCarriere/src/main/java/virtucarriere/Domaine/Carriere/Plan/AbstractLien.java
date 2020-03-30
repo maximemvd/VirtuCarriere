beb3a0f42f28db07816dec443f664db1cac712d1
Vincent Masse <vincent.masse.4@ulaval.ca>
2020-03-30 11:35:38 -0400

    Not an implementation of Lien as it's now useless

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Plan/AbstractLien.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Plan/AbstractLien.java
@@ -2,8 +2,7 @@ package virtucarriere.Domaine.Carriere.Plan;
 
 import java.awt.Point;
 
-public abstract class AbstractLien<EndType extends Element> extends Element
-    implements Lien<Element> {
+public abstract class AbstractLien<EndType extends Element> extends Element {
 
   public AbstractLien(Point point, int p_width, int p_length, boolean p_selectionStatus) {
     super(point, p_width, p_length, p_selectionStatus);
