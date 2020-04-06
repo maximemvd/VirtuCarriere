b623e44d1d4f88ab5a0bf104069fd04579ae4803
Vincent Masse <vincent.masse.4@ulaval.ca>
2020-04-06 17:10:38 -0400

    Add needed method to plan so we will pe able to use it in ElementContainer

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Plan/Plan.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Plan/Plan.java
@@ -1,38 +1,64 @@
 package virtucarriere.Domaine.Carriere.Plan;
 
+import java.awt.Point;
+
 public class Plan {
+
   GraphConvoyeur equipments = new GraphConvoyeur();
   GraphChemins chemins = new GraphChemins();
 
   void addArc(Arc arc) {
     chemins.addLink(arc);
-  };
+  }
+
+  void removeArc(Arc arc) {
+    chemins.removeLink(arc);
+  }
 
   void addBroyeur(Broyeur broyeur) {
-    equipments.addEnd(broyeur);
-  };
+    addEquipment(broyeur);
+  }
 
   void addConcasseur(Concasseur concasseur) {
-    equipments.addEnd(concasseur);
-  };
+    addEquipment(concasseur);
+  }
 
   void addConvoyeur(Convoyeur convoyeur) {
     equipments.addLink(convoyeur);
-  };
+  }
 
   void addCrible(Crible crible) {
-    equipments.addEnd(crible);
-  };
+    addEquipment(crible);
+  }
 
   void addEntree(Entree entree) {
     chemins.addEnd(entree);
-  };
+  }
 
-  void addNoeud(Noeud noeud) {
-    chemins.addEnd(noeud);
-  };
+  void removeRntree(Entree entree) {
+    chemins.removeEnd(entree);
+  }
 
   void addTas(Tas tas) {
-    equipments.addEnd(tas);
-  };
+    addEquipment(tas);
+  }
+
+  void addEquipment(Equipement equipement) {
+    // TODO validate dependency
+    // TODO validate that nothing is present in chemin graphe
+    equipments.addEnd(equipement);
+  }
+
+  boolean validateElementPresent(Point point) {
+    return false;
+  }
+
+  void addNoeud(Noeud noeud) {
+    // TODO validate element present
+    chemins.addEnd(noeud);
+  }
+
+  void removeNoeud(Noeud noeud) {
+    chemins.removeEnd(noeud);
+  }
 }
