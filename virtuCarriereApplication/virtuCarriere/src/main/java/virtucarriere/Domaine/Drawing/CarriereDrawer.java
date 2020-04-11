563d9b3f26ae203f1241fba99a18530da755cfcf
Maxime Miville Deschênes <maxime.miville-deschenes.1@ulaval.ca>
2020-04-11 10:07:14 -0400

    changement zoom sur point de chargement

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Drawing/CarriereDrawer.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Drawing/CarriereDrawer.java
@@ -82,8 +82,8 @@ public class CarriereDrawer {
       g2d.setColor(point.getColor());
       Point pointCharge = point.getPoint();
       g2d.fillOval((int) pointCharge.x - radius, (int) pointCharge.y - radius, radius, radius);
-      g2d.scale(1 / zoom, 1 / zoom);
     }
+    g2d.scale(1 / zoom, 1 / zoom);
   }
 
   // drawChargeur is good
