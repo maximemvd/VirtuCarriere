e6e6b08ce642615d4ae683681a1890e03f461caa
Philippe Vincent <philippe.vincent.3@ulaval.ca>
2020-04-23 19:16:07 -0400

    conveter Uni

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/AffichageUtil/UnitConverter.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/AffichageUtil/UnitConverter.java
@@ -4,19 +4,40 @@
  * and open the template in the editor.
  */
 package virtucarriere.Domaine.AffichageUtil;
+import java.awt.Point;
 
 /** @author vincentmasse */
 public class UnitConverter {
 
-  private static float PixelToMeter = (float) 0.1;
+  private static float converterUnit = (float) 0.1;
 
   public static double pixelToMeter(double pixel) {
     // TODO calcul à faire selon la dimension de notre fenêtre
-    return pixel * PixelToMeter;
+    return pixel * converterUnit;
   }
 
   public static double meterToPixel(double meter) {
     // TODO calcul à faire selon la dimension de notre fenêtre
-    return meter / PixelToMeter;
+    return meter / converterUnit;
+  }
+
+
+  public static float getConverterUnit(){
+    return converterUnit;
+  }
+
+  public static void setConverterUnit(float p_converter){
+    if (p_converter > 0){
+      converterUnit = p_converter;
+    }
+  }
+
+  public static Point convertPointToMeter(Point p_point) {
+    return new Point((int) (p_point.x * converterUnit), (int) (p_point.y * converterUnit));
+  }
+
+
+  public static Point convertPointToPixel(Point p_point) {
+    return new Point((int) (p_point.x / converterUnit), (int) (p_point.y/ converterUnit));
   }
 }
