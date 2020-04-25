b2bf2967fdecacd43cbf97dfcd68a21db2ad966b
Vincent Masse <vincent.masse.4@ulaval.ca>
2020-04-25 15:24:30 -0400

    Implement contains in AbstractPointChemin

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Plan/AbstractPointChemin.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Carriere/Plan/AbstractPointChemin.java
@@ -2,17 +2,26 @@ package virtucarriere.Domaine.Carriere.Plan;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
 
 public abstract class AbstractPointChemin extends Element {
 
   private Color color = null;
+  private int radius;
 
-  public AbstractPointChemin(Point point, int p_width, int p_length, Color color) {
-    super(point, p_width, p_length);
+  public AbstractPointChemin(Point point, int radius, Color color) {
+    super(point);
     this.color = color;
   }
 
   public Color getColor() {
     return color;
+  }
+
+  @Override
+  public boolean contains(double p_x, double p_y) {
+    Shape shape = new Ellipse2D.Double(point.getX(), point.getY(), radius, radius);
+    return shape.contains(p_x, p_y);
   }
 }
