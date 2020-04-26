c04ff643096fef09a75e3e5f5e020e9bf623ba46
Francis Boulianne <francis.boulianne.2@ulaval.ca>
2020-04-26 15:46:39 -0400

    ajout set simulation

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Controller/ElementContainer.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/Domaine/Controller/ElementContainer.java
@@ -32,6 +32,18 @@ public class ElementContainer implements Serializable, Observer, Observable {
   static File file;
   private URL url = null;
   private List<Observer> observerList = new ArrayList<>();
+  
+  public Simulation getSimulation(){
+    return this.simulation;
+  }
+  
+  public void setCamionList(List<Camion> camionList){
+    simulation.setCamionList(camionList);
+  }
+  
+  public void setChargeurList(List<Chargeur> chargeurList){
+    simulation.setChargeurList(chargeurList);
+  }
 
   public void initObserver(Controller controller) {
     addObserver(controller);
