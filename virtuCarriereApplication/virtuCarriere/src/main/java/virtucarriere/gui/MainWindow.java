32d9ec08ea875321b776231336a8dd3afc939348
Francis Boulianne <francis.boulianne.2@ulaval.ca>
2020-04-26 20:41:21 -0400

    mouse fixed

--- virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/gui/MainWindow.java
+++ virtuCarriereApplication/virtuCarriere/src/main/java/virtucarriere/gui/MainWindow.java
@@ -2169,10 +2169,8 @@ public class MainWindow extends JFrame {
 
   private void drawingPanelMouseDragged(java.awt.event.MouseEvent evt) {
     Point mousePoint = evt.getPoint();
-    double xCoord = UnitConverter.pixelToMeter(mousePoint.getX());
-    double yCoord = UnitConverter.pixelToMeter(mousePoint.getY());
-    UnitConverter.pixelToMeter(xCoord);
-    UnitConverter.pixelToMeter(yCoord);
+    double xCoord = mousePoint.getX();
+    double yCoord = mousePoint.getY();
     drawingPanel.setMouseX(xCoord);
     drawingPanel.setMouseY(yCoord);
     drawingPanel.repaint();
